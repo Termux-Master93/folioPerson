bc4c2844f57c3dde56ac6e5189dff9a60e225405
Termux-Master93 <brisantur8034@gmail.com>
2022-10-07 22:27:24 -0500

    actualizar dependencias y instalar librerias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { BrowserRouter,Route,Routes} from "react-router-dom";
-
-import Ingresar from './components/auth/Ingresar.jsx';
-import Auth from './components/auth/Auth.jsx';
-import HomeUser from './components/home/HomeUser.jsx';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from './components/Folio/LandingPage.jsx';
-
 import './App.css';
+
 function App() {
   return (
     <>
       <div className="App">
-        <BrowserRouter>
-                <Routes>
-                    <Route path='' element={<LandingPage/>}/>
-                    <Route path='' element={<Ingresar/>} />
-                    <Route path='/Auth' element={<Auth/>} />   
-                    <Route path='/HomeUser' element={<HomeUser/>} /> 
-                </Routes>
-        </BrowserRouter>
+          <BrowserRouter>
+              <Routes>
+                <Route  path='' element={<LandingPage/>}  />
+              </Routes>
+          </BrowserRouter>
+       
+
       </div>
     </>
 
